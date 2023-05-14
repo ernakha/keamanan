f869dc36a5d8f2a98d10c515fe9537cf94c8f12a
ajil23 <azrilpraya@gmail.com>
2023-05-14 13:45:39 +0800

    CRUD-done

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+  $host = "localhost";
+  $user = "root";
+  $pass = "";
+  $nama_db = "database"; //nama database
+  $koneksi = mysqli_connect($host,$user,$pass,$nama_db); //pastikan urutan nya seperti ini, jangan tertukar
+?>
